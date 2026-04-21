1d4b47abbfdc245ac6e920936c55fdbf3c8767b6
420-Paliwal <harshpaliwal0420@gmail.com>
2026-04-21 16:35:44 +0530

    add core functionality of file upload

--- index.js
+++ index.js
@@ -11,11 +11,31 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+// storage config
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 })
 
+app.post('/api/submit', upload.single("file"), (req, res) => {
+    const { name, email, message } = req.body;
+
+    console.log("DATA", name, email, message);
+    console.log("FILE", req.file);
+
+    res.json({ success: true, message: "File uploaded successfully!" });
+    
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
