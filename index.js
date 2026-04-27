2292721ebda12c2e49d4ab3aad8112b68d6e8428
420-Paliwal <harshpaliwal0420@gmail.com>
2026-04-27 14:58:38 +0530

    minio port error conflict resolve

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/api/submit', upload.single("file"), async (req, res) => {
     const Minio = require("minio");
     const minioClient = new Minio.Client({
       endPoint: process.env.MINIO_ENDPOINT,
-      port: process.env.MINIO_PORT || 9000,
+      port: Number(process.env.MINIO_PORT) || 9000,
       useSSL: false,
       accessKey: process.env.MINIO_ACCESS_KEY,
       secretKey: process.env.MINIO_SECRET_KEY,
